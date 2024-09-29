23f70303557eb04ff91f83f8531be0fc2773f60c
Vibhas-Akula <vibhas567@gmail.com>
2024-09-29 10:52:12 +0530

    LFS commit

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41107e14c7a54d7ae3749e3a8e9198305944c4de089671e0f413272c73d3e29e
+size 58
---
+++ api/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b408aafba2d17b84ac764056a4fa58e58a9322cc2785020bbba4698bd2142bc1
+size 2394
